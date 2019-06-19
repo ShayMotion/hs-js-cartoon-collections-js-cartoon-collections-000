94db6ea90f42b4819d0203e14966efa42d860f41
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-06-19 03:46:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var dwarfRollCall = ["Doc", "Dopey", "Bashful", "Grumpy"];
 }
 
 var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
-
 planeteerCalls.toUpperCase();
 
 longPlaneteerCalls(); {
